2d886382be2a39e50f1b8df9e14b0b91b99f4a2d
Qaizx <kaothom@gmail.com>
2021-07-25 17:21:32 +0700

    เหลือ local

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
 
 
 
-// const clickCallback = () => {
-//     const inputElem = document.querySelector('input')
-//     const ul = document.querySelector('ul')
-//     if(inputElem.value === ""){
-//         alert("Need Input!!")
-//     }else{
-//         // const ps = document.querySelector('dt')
-//         // const dt = document.createElement('dt')
-//         // if(ps === null){
-//         //     console.log(inputElem.value);
-//         //     dt.innerHTML = inputElem.value
-            
-//         // }else{
-//         //     // console.log(inputElem.value);
-//         //     dt.innerHTML = inputElem.value
-//         //     const ps = document.querySelectorAll('dt')
-//         //     console.log(dt);
-//         //     for(const p of ps){ 
-//         //         p.innerText = inputElem.value
-//         //     }
-//         // }
-//         // dl.append(dt)
-//         console.log(inputElem.value);
-//         const li = document.createElement('li')
-//         li.innerHTML = inputElem.value
-//         ul.appendChild(li)
+const clickCallback = () => {
+    const inputElem = document.querySelector('input')
+    const dt = document.querySelector('dt')
+    const ul = document.querySelector('ul')
+    const mainDiv = document.createElement('div')
+    if(inputElem.value === ""){
+        alert("Need Input!!")
+    }else{
 
-//     }
+        console.log(inputElem.value);
+        const dl = document.createElement('dl')
+        const li = document.createElement('li')
+        li.innerHTML = inputElem.value
 
-// }
+        const delBtn = document.createElement('button')
+        delBtn.innerHTML = 'delete'
+        delBtn.addEventListener('click', () => {
+            mainDiv.removeChild(li)
+        })
+
+        const donBtn = document.createElement('button')
+        donBtn.innerHTML = 'done'
+        donBtn.addEventListener('click', () => {
+            dl.append(li)
+            mainDiv.removeChild(li)
+        })
+        
+        li.append(donBtn)
+        li.append(delBtn)
+        mainDiv.append(li)
+        ul.insertBefore(mainDiv, ul.childNodes[0])
+        dt.insertBefore(dl, dt.childNodes[0])
+
+    }
+
+}
 
 const keyEnter = (event) => {
     const dt = document.querySelector('dt')
     const ul = document.querySelector('ul')
     const mainDiv = document.createElement('div')
+    const buttonDiv = document.createElement('div')
+    buttonDiv.setAttribute("class", "flex justify-end")
 
     if( event.keyCode == 13 ) {
         const inputElem = document.querySelector('input')
         if(inputElem.value === ""){
             alert("Need Input!!")
         }else{
-            console.log(inputElem.value);
+
             const dl = document.createElement('dl')
             const li = document.createElement('li')
             li.innerHTML = inputElem.value
-
             const delBtn = document.createElement('button')
-            delBtn.innerHTML = 'delete'
+            delBtn.innerHTML = '<img width="25" height="25" src="https://image.flaticon.com/icons/png/512/1828/1828843.png" alt="Remove free icon" title="Remove free icon" class="loaded">'
             delBtn.addEventListener('click', () => {
                 mainDiv.removeChild(li)
 	        })
 
             const donBtn = document.createElement('button')
-            donBtn.innerHTML = 'done'
+            donBtn.setAttribute("class", "mr-3")
+            donBtn.innerHTML = '<img width="25" height="25" src="https://image.flaticon.com/icons/png/512/190/190411.png" alt="Checked free icon" title="Checked free icon" class="loaded">'
             donBtn.addEventListener('click', () => {
                 dl.append(li)
                 mainDiv.removeChild(li)
 	        })
             
-            li.append(donBtn)
-            li.append(delBtn)
+            buttonDiv.append(donBtn)
+            buttonDiv.append(delBtn)
+            li.append(buttonDiv)
             mainDiv.append(li)
             ul.insertBefore(mainDiv, ul.childNodes[0])
             dt.insertBefore(dl, dt.childNodes[0])
